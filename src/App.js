87d6ee61b9325486b9db45d87decb81659edbe52
yurimlee <yuuthod@we-gokorea.com>
2020-09-24 16:16:51 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useRef, useState, useCallback, useMemo } from 'react';
-import './App.css';
+import React, { useRef, useMemo, useReducer, useCallback } from 'react';
 
 import UserList from './UserList';
 import CreateUser from './CreateUser';
@@ -9,28 +8,17 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
     email: ''
-  });
-
-  const { username, email } = inputs;
-
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    setInputs(inputs => ({
-      ...inputs,
-      [name]: value
-    }));
-  },[]);
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
-        id: 1,
-        username: 'test1',
-        email: 'test1@gmail.com',
-        active: false
+      id: 1,
+      username: 'test1',
+      email: 'test1@gmail.com',
+      active: false
     },
     {
         id: 2,
@@ -44,42 +32,90 @@ function App() {
         email: 'test3@gmail.com',
         active: false
     },
-  ]);
+  ]
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHAGNE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      }
+    case 'CREATE_USER':
+      console.log(action.user);
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      }
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user => 
+          user.id === action.id ? { ...user, active: !user.active } : user
+        )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [ state, dispatch ] = useReducer(reducer, initialState);
   const nextId = useRef(4);
+
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHAGNE_INPUT',
+      name,
+      value
+    });
+  }, []);
+
   const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-
-    setUsers(users => users.concat(user));
-
-    setInputs({
-      username: '',
-      email: ''
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
     });
     nextId.current += 1;
-  }, [username, email]);
-
-  const onRemove = useCallback(id => {
-    setUsers(users.filter(user => user.id !== id));
-  }, [users]);
+  }, [username, email])
 
   const onToggle = useCallback(id => {
-    setUsers(users => users.map((user) => user.id === id ? {...user, active: !user.active} : user))
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
   },[]);
 
-  const count = useMemo(() => countActiveUsers(users), [users])
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
     <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
       <p>활설화된 사용자 수 : {count}</p>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Counter from './Counter';
+import App from './App';
+// import Counter from './Counter';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   // <React.StrictMode>
-    <Counter />,
+    // <Counter />,
+    <App />,
   // </React.StrictMode>,
   document.getElementById('root')
 );
