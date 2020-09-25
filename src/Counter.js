1cd18eefdb15ddef81fcd736e22fdecf2ce7f8b9
yurimlee <yuuthod@we-gokorea.com>
2020-09-25 14:29:39 +0900

    class setState

--- src/Counter.js
+++ src/Counter.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.handleIncrease = this.handleIncrease.bind(this);
-        this.handleDecrease = this.handleDecrease.bind(this);
+    state = {
+        counter: 0
+    };
+
+    handleIncrease = () => {
+        // setState는 단순히 상태를 바꿔달라고 요청을 해주고,
+        // 성능적인 이유 때문에 상태가 바로 업데이트 되지 않고
+        // 비동기적으로 업데이트가 됩니다.
+        // 때문에 setSate를 여러변 요청하고 싶다면, 다시 화살표 함수를 써서 사용해야 합니다.
+        this.setState(state => ({
+            counter: state.counter + 1
+        }));
+        this.setState(state => ({
+            counter: state.counter + 1
+        }));
+    };
+
+    // 만약 업데이트가 되고 나서 어떤 작업을 하고 싶다면 다음과 같이 
+    // setState의 두번째 파라미터에 콜백함수를 넣어줄 수도 있습니다.
+    towice = () => {
+        this.setState(
+            {
+                counter: this.state.counter + 1
+            },
+            () => {
+                console.log(this.state.counter);
+            }
+        )
     }
 
-    handleIncrease() {
-        console.log("increase");
-        console.log(this);
-    }
-
-    handleDecrease() {
-        console.log("decrease");
+    handleDecrease = () => {
+        this.setState({
+            counter: this.state.counter - 1
+        });
     }
  
     render() {
         return (
             <div>
-                <h1>0</h1>
+                <h1>{this.state.counter}</h1>
                 <button onClick={this.handleIncrease}>+1</button>
                 <button onClick={this.handleDecrease}>-1</button>
             </div>
