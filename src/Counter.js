e445ecdc27ab80b90a2671dc1dab4e75c371a353
yurimlee <yuuthod@we-gokorea.com>
2020-10-12 15:21:21 +0900

    last

--- src/Counter.js
+++ src/Counter.js
@@ -23,7 +23,7 @@ function Counter() {
 
     return (
         <div>
-            <h1>{number}</h1>
+            <h1>{number}</h1> 
             <button onClick={onIncrease}>+1</button>
             <button onClick={onDecrease}>-1</button>
         </div>
